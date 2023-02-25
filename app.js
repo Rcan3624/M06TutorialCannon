7bff74b42487c16998ba24599d6389017941d5f9
Rcan3624 <55251953+Rcan3624@users.noreply.github.com>
2023-02-25 16:23:17 -0500

    Finished Lesson 9

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
+// connect to mongodb
+const dbURI = 'mongodb+srv://rcannon13:Nc28e7DXOlbqJjuf@nodetuts.p9cxa1e.mongodb.net/node-tuts?retryWrites=true&w=majority'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 // register view engine
 app.set('view engine', 'ejs');
-// app.set('views', 'myviews');
-
-// listen for requests
-app.listen(3000);
 
 // middleware & static files
 app.use(express.static('public'));
-app.use(morgan('dev'))
-
+app.use(morgan('dev'));
+      
+// routes
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-  res.render('index', { title: 'Home', blogs });
+  res.redirect('/ blogs');
 });
 
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
+
+// blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('index', { title: 'All Blogs', blogs: result })
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
 
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
@@ -37,3 +48,6 @@ app.get('/blogs/create', (req, res) => {
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
+
+// https://www.youtube.com/watch?v=bxsemcrY4gQ&t=771s
+// 12:46
