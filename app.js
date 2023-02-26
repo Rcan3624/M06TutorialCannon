db42c868f59b67896ab8c40ae38f688e3128abdf
Rcan3624 <55251953+Rcan3624@users.noreply.github.com>
2023-02-25 19:40:11 -0500

    Finished Lesson 10

--- app.js
+++ app.js
@@ -8,22 +8,22 @@ const app = express();
 
 // connect to mongodb
 const dbURI = 'mongodb+srv://rcannon13:Nc28e7DXOlbqJjuf@nodetuts.p9cxa1e.mongodb.net/node-tuts?retryWrites=true&w=majority'
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-      
+
 // routes
 app.get('/', (req, res) => {
-  res.redirect('/ blogs');
+  res.redirect('/blogs');
 });
-
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
@@ -32,13 +32,70 @@ app.get('/about', (req, res) => {
 // blog routes
 app.get('/blogs', (req, res) => {
   Blog.find().sort({ createdAt: -1 })
+    .then(result => {
+      res.render('index', { blogs: result, title: 'All blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog.save()
     .then((result) => {
-      res.render('index', { title: 'All Blogs', blogs: result })
+      res.redirect('/blogs');
     })
     .catch((err) => {
       console.log(err);
     })
 })
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id
+  Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+})
+
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/single-blog', (req, res) => {
+  Blog.findById('5ea99b49b8531f40c0fde689')
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+  .then(result => {
+    res.json({ redirect: '/blogs'})
+  })
+  .catch(err => {
+    console.log(err); 
+  })
+})
+
+
 
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
@@ -48,6 +105,3 @@ app.get('/blogs/create', (req, res) => {
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
-
-// https://www.youtube.com/watch?v=bxsemcrY4gQ&t=771s
-// 12:46
